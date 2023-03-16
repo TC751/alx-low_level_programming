c913ab5c4df8750f954a1585f6775d7923b9a230
TC751 <teddychiingujr@gmail.com>
2023-03-16 07:02:32 -0700

    4-puts.c edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,10 +10,10 @@
  *                puzzle, followed by a new line.
  *
  * Return: Always 0.
- i*/
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
