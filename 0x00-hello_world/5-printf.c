f39396125cb2b248e5c22fdf33b1bedb56e877e0
TC751 <teddychiingujr@gmail.com>
2023-03-16 07:14:51 -0700

    5-print edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,16 @@
 /*
- * file: 5-print.c
- * author: teddy chingu
+ * File: 5-printf.c
+ * Auth: teddy
  */
 
 #include <stdio.h>
 
-/*
- * main prints "with proper grammer, but the outcome is a peice of art"
- * 			followed by a new line.
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
- * return will always be 0.
+ * Return: Always 0.
  */
- 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
