88517fb74083b20c5c5fb1f98ecb3e69f3df86b1
TC751 <teddychiingujr@gmail.com>
2023-03-22 05:15:42 -0700

    main betty

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
-void positive_or_negative(int i) {
+/**
+ * positive_or_negative - checks if a number is positive or negative or zero
+ *
+ *
+ * int i - is an integer that is given as a parameter
+ */
+
+void positive_or_negative(int i)
+{
 	if (i > 0)
 		printf("%d is positive\n", i);
 	else if (i < 0)
