5413883f3d956caa09021d878be45f5e2e462db7
TC751 <teddychiingujr@gmail.com>
2023-04-05 12:16:02 -0700

    4-pow.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1);
-    else if (y == 0)
-        return (1);
-    else if (y % 2 == 0)
-        return _pow_recursion(x * x, y / 2);
-    else
-        return x * _pow_recursion(x, y - 1);
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y % 2 == 0)
+		return _pow_recursion(x * x, y / 2);
+	else
+		return x * _pow_recursion(x, y - 1);
 }
